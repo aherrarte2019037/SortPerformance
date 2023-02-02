ff94b60feb523492028f158ba0de5e9a07cbd7d8
Angel <angelherrarte3@gmail.com>
2023-02-02 08:07:54 -0600

    progress

--- src/main/java/com/sortperformance/Profiler.java
+++ src/main/java/com/sortperformance/Profiler.java
@@ -17,4 +17,22 @@ public class Profiler {
 
         return timeElapsedMicroseconds;
     }
+
+    public static Long testAlreadyOrdered(int numbersCount, Sorter sorter) {
+        ArrayList<SortItem> list = new ArrayList<SortItem>();
+        for (int index = 0; index < numbersCount; index++) {
+            list.add(new SortItem(index + 1));
+        }
+
+        long start = System.nanoTime();
+
+        sorter.execute(list);
+
+        long finish = System.nanoTime();
+
+        long timeElapsed = finish - start;
+        long timeElapsedMicroseconds = TimeUnit.MICROSECONDS.convert(timeElapsed, TimeUnit.NANOSECONDS);
+
+        return timeElapsedMicroseconds;
+    }
 }
