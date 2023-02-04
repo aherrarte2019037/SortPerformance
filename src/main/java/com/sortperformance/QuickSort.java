e7fdb2335e232d8f6758f52f415dfafac1855cfe
Czar272 <110710742+Czar272@users.noreply.github.com>
2023-02-04 15:08:18 -0600

    Add files via upload

---
+++ src/main/java/com/sortperformance/QuickSort.java
@@ -0,0 +1,60 @@
+package main.java.com.sortperformance;
+
+import java.util.ArrayList;
+
+import javax.lang.model.util.ElementScanner14;
+
+@SuppressWarnings("unchecked")
+public class QuickSort implements Sorter {
+
+    public ArrayList<SortItem> execute(ArrayList<SortItem> list){
+        ArrayList<SortItem> listClone = (ArrayList<SortItem>) list.clone();
+
+        listClone = quicksort1(listClone);
+
+    }
+
+    public int[] quicksort1(int numeros[]){
+
+        return quicksort2(numeros, 0, numeros.length-1);
+    }
+
+    public int[] quicksort2(int numeros[], int izq, int der){
+
+        if(izq >= der)
+            return numeros;
+        int i = izq, d = der;
+        if (izq!= der){
+
+            int pivote;
+            int aux; 
+            pivote = izq;
+            while(izq!=der){
+
+                while(numeros[der]>=numeros[pivote] && izq<der)
+                    der --;
+                while(numeros[izq]<numeros[pivote] && izq<der)
+                    izq++;
+
+                if(der != izq){
+
+                    aux = numeros[der];
+                    numeros[der] = numeros[izq];
+                    numeros[izq] = aux;}
+                }
+
+            if(izq == der){
+
+                quicksort2(numeros, i, izq-1);
+                quicksort2(numeros, izq+1, d);
+            }
+        }
+
+        else
+            return numeros;
+        return numeros;
+            
+
+    }
+         
+}
